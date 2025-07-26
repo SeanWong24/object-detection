5b360b9f811f98b3e65ea53134695f549881ada4
Sean Wong <seanwong24@live.com>
2025-07-25 23:35:48 -0700

    modify GPU detection

--- detector.util.js
+++ detector.util.js
@@ -2,10 +2,22 @@ import { env, pipeline } from "https://esm.run/@huggingface/transformers";
 
 env.allowLocalModels = false;
 
+
+async function isWebGPUAvailable() {
+  if (!navigator.gpu) return false;
+
+  try {
+    const adapter = await navigator.gpu.requestAdapter();
+    return adapter !== null;
+  } catch (e) {
+    return false;
+  }
+}
+
 export const detector = await pipeline(
   "object-detection",
   "Xenova/detr-resnet-50",
   {
-    device: navigator.gpu ? "webgpu" : "wasm",
+    device: (await isWebGPUAvailable()) ? "webgpu" : "wasm",
   }
 );
